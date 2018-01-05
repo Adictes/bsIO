788a3ef661c0eebf33219dc91bea073bc606ed81
Vyacheslav <sime_pel@mail.ru>
2018-01-05 16:16:44 +0700

    Add example with template

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var homeTmp *template.Template
+
+// Context is struct for template
+type Context struct {
+	Title string
+	Name  string
+}
+
 func main() {
 	http.HandleFunc("/", Greetings)
+
+	t := template.Must(template.New("Game").ParseFiles("template.html"))
+	homeTmp = t.Lookup("template.html")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -16,5 +28,5 @@ func main() {
 
 // Greetings says hello in browser's window
 func Greetings(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, my dear friends!"))
+	homeTmp.Execute(w, Context{"Doc", "Alesha"})
 }
